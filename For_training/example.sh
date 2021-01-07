5dd22a1c116d986489d3414c478f125dbefcc99a
Shuo Shi <chishuo16@sina.com>
2021-01-07 10:04:20 +0800

    Update example.sh

--- For_training/example.sh
+++ For_training/example.sh
@@ -1,1 +1,1 @@
-python3 HGImp_ToBeTrained.py example/test.sample_snp.matrix.bz2 example/test.sample_population.matrix example/test.out
+python3 RefRGim_ToBeTrained.py example/test.sample_snp.matrix.bz2 example/test.sample_population.matrix example/test.out
